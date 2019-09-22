f12eb68168b88a8a4454d378eabeac39ef86c6c8
Enrique Mora <enriquebeta7@gmail.com>
2019-09-22 18:53:46 -0300

    Refactor of Calculator lib and tests to ES6 code.

--- calculator/src/js/index.js
+++ calculator/src/js/index.js
@@ -1,16 +1,11 @@
-var calculator = {
-  sum: function(n1, n2) {
-    return n1 + n2;
-  },
-  subtract: function(n1, n2) {
-    return  n1 - n2;
-  },
-  multiplication: function(n1, n2) {
-    return  n1 * n2;
-  },
-  division: function(n1, n2) {
-    return n2 === 0 ? 'It is not possible to divide by 0' : n1 / n2;
-  }
-}
+const sum = (n1, n2) => n1 + n2;
+const subtract = (n1, n2) => n1 - n2;
+const multiplication = (n1, n2) => n1 * n2;
+const division = (n1, n2) => n2 === 0 ? 'It is not possible to divide by 0' : n1 / n2;
 
-module.exports = calculator;
+export default {
+  sum,
+  subtract,
+  multiplication,
+  division
+};
--- calculator/src/tests/index.test.js
+++ calculator/src/tests/index.test.js
@@ -1,61 +1,61 @@
-var expect = require('chai').expect;
-var calculator = require('../../src/js/index');
+import  { expect } from 'chai';
+import calculator from '../../src/js/index';
 
-describe('Calculator', function() {
-  describe('Smoke Tests', function() {
-    it('Should exist the Calculator lib', function() {
+describe('Calculator', () => {
+  describe('Smoke Tests', () => {
+    it('Should exist the Calculator lib', () => {
       expect(calculator).to.exist;
     });
 
-    it('Should exist the method sum', function() {
+    it('Should exist the method sum', () => {
       expect(calculator.sum).to.exist;
       expect(calculator.sum).to.be.a('function');
     });
 
-    it('Should exist the method subtract', function() {
+    it('Should exist the method subtract', () => {
       expect(calculator.subtract).to.exist;
       expect(calculator.subtract).to.be.a('function');
     });
 
-    it('Should exist the method multiplication', function() {
+    it('Should exist the method multiplication', () => {
       expect(calculator.multiplication).to.exist;
       expect(calculator.multiplication).to.be.a('function');
     });
 
-    it('Should exist the method division', function() {
+    it('Should exist the method division', () => {
       expect(calculator.division).to.exist;
       expect(calculator.division).to.be.a('function');
     });
   });
 
-  describe('Method sum', function() {
-    it('Should return 4 when sum(2,2)', function() {
+  describe('Method sum', () => {
+    it('Should return 4 when sum(2,2)', () => {
       expect(calculator.sum(2,2)).to.be.equal(4);
     });
   });
 
-  describe('Method subtract', function() {
-    it('Should return 0 when subtract(2,2)', function() {
+  describe('Method subtract', () => {
+    it('Should return 0 when subtract(2,2)', () => {
       expect(calculator.subtract(2,2)).to.be.equal(0);
     });
 
-    it('Should return -2 when subtract(2,4)', function() {
+    it('Should return -2 when subtract(2,4)', () => {
       expect(calculator.subtract(2,4)).to.be.equal(-2);
     });
   });
 
-  describe('Method multiplication', function() {
-    it('Should return 4 when multiplication(2,2)', function() {
+  describe('Method multiplication', () => {
+    it('Should return 4 when multiplication(2,2)', () => {
       expect(calculator.multiplication(2,2)).to.be.equal(4);
     });
   });
 
-  describe('Method division', function() {
-    it('Should return 1 when division(2,2)', function() {
+  describe('Method division', () => {
+    it('Should return 1 when division(2,2)', () => {
       expect(calculator.division(2,2)).to.be.equal(1);
     });
 
-    it('Should return "It is not possible to divide by 0" when divide by 0', function() {
+    it('Should return "It is not possible to divide by 0" when divide by 0', () => {
       expect(calculator.division(2,0)).to.be.equal('It is not possible to divide by 0');
     });
   });
