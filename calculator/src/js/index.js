a2ba39e806df76018bd992404c7ce24abde41f5c
Enrique Mora <enriquebeta7@gmail.com>
2019-09-28 21:58:23 -0300

    Added eslint, eslint settings, "lint" npm script.
      - Added eslint.
      - Added eslint settings.
      - Added the npm script "lint" for run the linter.
      - added small changes to follow the eslint rules.

--- calculator/src/js/index.js
+++ calculator/src/js/index.js
@@ -1,11 +1,11 @@
 const sum = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
 const multiplication = (n1, n2) => n1 * n2;
-const division = (n1, n2) => n2 === 0 ? 'It is not possible to divide by 0' : n1 / n2;
+const division = (n1, n2) => (n2 === 0 ? 'It is not possible to divide by 0' : n1 / n2);
 
 export default {
   sum,
   subtract,
   multiplication,
-  division
+  division,
 };
--- fizzBuzz/src/js/index.js
+++ fizzBuzz/src/js/index.js
@@ -1,10 +1,11 @@
-const FizzBuzz  = number => {
+// eslint-disable-next-line
+const FizzBuzz = number => {
   if (number === 0) return 0;
   if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
   if (number % 3 === 0) return 'Fizz';
   if (number % 5 === 0) return 'Buzz';
 
   return number;
-}
+};
 
 export default FizzBuzz;
